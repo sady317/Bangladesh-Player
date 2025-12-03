85eb54eb69bdb83ff5b705b244697f2177996bd0
Sady317 <sadychowdhury92@gmail.com>
2025-12-03 20:44:35 +0600

    add extra interactive things

--- src/Home/home.jsx
+++ src/Home/home.jsx
@@ -5,12 +5,20 @@ import PlayerCard from '../components/PlayerCard'
 export default function Home() {
 
   const [player,setPlayer]=useState([ ])
-  console.log(player)
 
+  const [selectPlayer,setSelectPlayer]=useState([])
+  // console.log(selectPlayer)
+
+function addPlayer(details){
+  const newPlayer=[...selectPlayer,details]
+  setSelectPlayer(newPlayer)
+
+  
+}
 
 
   useEffect(()=>{
-    fetch("./public/data/player.json")
+    fetch("./player.json")
     .then((res)=>res.json())
     .then((data)=>setPlayer(data))
   },[])
@@ -19,23 +27,35 @@ export default function Home() {
 
   return (
     <>
-    <h1 className='text-center bg-red-400 w-30 cursor-pointer mx-auto my-6 py-2 text-white rounded-sm'>Make your 11</h1>
+    <h1  className='text-center bg-red-400 w-30 cursor-pointer mx-auto my-6 py-2 text-white rounded-sm'>Make your 11</h1>
     
-    <section  className='flex '>
-    <div className='grid grid-cols-4 space-y-8 '>
+    
+    <section  className='grid grid-cols-10'>
+    <div className='grid grid-cols-3 space-y-8 col-span-8'>
       {player.map((p)=>(
-        <PlayerCard player={p}/>
+        <PlayerCard player={p} addPlayer={addPlayer}/>
  
       ))}
         
 </div>
 
-
-<div className='w-[50%] bg-gray-400 rounded-md p-12 '>
-<ul  id='display' className='list-decimal'>
-{/* <li id='child'></li> */}
-</ul>
+<div className='col-span-2'>
+<h1 className='text-center bg-red-400  cursor-pointer  mb-6 py-2 text-white rounded-sm'>Selected Players</h1>
+<div>
+{selectPlayer.map((p)=>(
+  <>
+  <div className='flex items-center gap-2 space-y-4'>
+    <img src={p.img} alt="" className='w-10 h-10' />
+  <p>{p.name}</p>
+  </div>
+  </>
+))}
 </div>
+</div>
+
+
+
+
 </section>
     </>
   )
--- src/components/PlayerCard.jsx
+++ src/components/PlayerCard.jsx
@@ -1,29 +1,26 @@
 import React, { useState } from 'react'
 
-export default function PlayerCard({player}) {
+export default function PlayerCard({player,addPlayer}){
+  
+
+  
   return (
    <>
 
            <div className="card bg-base-100 w-[90%] mx-auto shadow-sm  border px-2 py-1">
+
   <figure>
-    <img
-      src={player.img}
-      alt="Shoes" />
+
+    <img  src={player.img}  alt="Shoes" />
+
   </figure>
+
   <div className="card-body">
     <h2 className="card-title">{player.name}</h2>
     <p>{player.description}</p>
     <div className="card-actions justify-end">
      
-      <button id='btn'  onClick={()=>{
-        const display=document.getElementById("display")
-        const li=document.createElement("li")
-        li.classList.add("text-xl")
-        li.innerText=player.name
- 
-        display.appendChild(li)
-
-      }} className="btn btn-primary">Select</button>
+      <button onClick={()=>{addPlayer(player)}} className="btn btn-primary">Select</button>
       
 
     </div>
